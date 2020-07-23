e268939a03bc907cdc8fb2cc9b1ac75bcf6206bc
PavanKumar Kavvuri <PavanproJack@github.com>
2020-07-23 17:07:23 +0100

    Initialised the Print board function

--- Intro2C++/main.cpp
+++ Intro2C++/main.cpp
@@ -6,10 +6,35 @@
 //  Copyright © 2020 PavanKumar. All rights reserved.
 //
 
+#include <cstdio>
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+void PrintBoard(vector<vector<int>> board){
+    
+    for(int i = 0; i < board.size(); i++){
+        for(int k = 0; k < board[i].size(); k++){
+            cout << board[i][k] ;
+        }
+         cout<<"\n";
+    }
+}
+
+int main()
+{
+    vector<vector<int>> board;
+    
+    board = {
+                {0, 1, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0}
+        
+            };
+    PrintBoard(board);
     return 0;
 }
+
