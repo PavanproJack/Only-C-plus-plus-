59889d2f7214143febe6e8ef399d7df552ef9c06
PavanKumar Kavvuri <PavanproJack@github.com>
2020-07-31 10:37:20 +0100

    A* a little change

--- A-Star Search/main.cpp
+++ A-Star Search/main.cpp
@@ -7,9 +7,94 @@
 //
 
 #include <iostream>
+#include <sstream>
+#include <fstream>
+
+#include <vector>
+#include <string>
+
+
+using std::vector;
+using std::string;
+using std::ifstream;
+using std::istringstream;
+using std::cout;
+using std::endl;
+
+
+enum class state{
+    Empty,
+    Obstacle
+};
+
+vector<vector<state>> Search(vector<vector<state>> board, int start[2], int goal[2]){
+    
+    vector<vector<state>> path{};
+    cout<<"No Path found"<<endl;
+    return path;
+}
+
+vector<state> parseNewLine(string line){
+    vector<state> row;
+    for(auto i : line){
+        //row.push_back(i==0? state::Empty : state::Obstacle);
+        cout<< i;
+    }
+    return row;
+}
+
+vector<vector<state>> ReadBoardFile(string path){
+    
+    vector<vector<state>> board;
+    ifstream ifs;
+    ifs.open(path, ifstream::in);
+    if (ifs.is_open()){
+        string line;
+        while(getline(ifs, line)){
+            vector<state> row = parseNewLine(line);
+            //board.push_back(row);
+        }
+    }
+    return board;
+}
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    string boardPath = "map.board";
+    ReadBoardFile(boardPath);
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
