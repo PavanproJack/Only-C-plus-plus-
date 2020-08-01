d4b0d8539154d1c04d6fc1fbbd3c19f53ad9075f
PavanKumar Kavvuri <PavanproJack@github.com>
2020-08-01 15:26:48 +0100

    Fixes the bug BAD::ACCESS

--- A-Star Search/main.cpp
+++ A-Star Search/main.cpp
@@ -32,6 +32,12 @@ enum class state{
     Path
 };
 
+ 
+void print(std::string statement){
+    std::cout<< statement << std::endl;
+}
+
+
 bool Compare(vector<int> openNode1, vector<int> openNode2){
     int f_node1 = openNode1[2] + openNode1[3];
     int f_node2 = openNode2[2] + openNode2[3];
@@ -51,7 +57,6 @@ string CellString(state cell){
 }
 
 void PrintBoard(vector<vector<state>> board){
-    cout<<board.size();
     for(auto i :  board){
         for(auto v : i){
             cout<< CellString(v);
@@ -61,7 +66,7 @@ void PrintBoard(vector<vector<state>> board){
 }
 
 void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &open, vector<vector<state>> &grid){
-    cout<<x<<"\n"<<y;
+    
     open.push_back(vector<int>{x, y, g, h});
     grid[x][y] = state::Closed;
     
@@ -72,10 +77,11 @@ int Heuristic(int x1, int y1, int x2, int y2){
 }
 
 bool CheckValidCell(int x, int y, vector<vector<state>> grid){
-    if(grid[x][y] == state::Empty)
-        return true;
-    else
-        return false;
+    bool on_grid_x = (x >= 0 && x < grid.size());
+    bool on_grid_y = (y >= 0 && y < grid[0].size());
+    if (on_grid_x && on_grid_y)
+        return grid[x][y] == state::Empty;
+    return false;
 }
 
 void ExpandNeighbors(vector<int> &current, vector<vector<int>> &open, vector<vector<state>> &grid, int goal[2]){
@@ -84,14 +90,14 @@ void ExpandNeighbors(vector<int> &current, vector<vector<int>> &open, vector<vec
     int x = current[0];
     int y = current[1];
     int g = current[2];
-    int h = Heuristic(x, y, goal[0], goal[1]);
+    //int h = Heuristic(x, y, goal[0], goal[1]);
     
     for(auto i : delta1){
         int x2 = x + i[0];
         int y2 = y + i[1];
-        if(CheckValidCell(x, y, grid)){
+        if(CheckValidCell(x2, y2, grid)){
             int g2 = g + 1;
-            int h2 = Heuristic(x, y, goal[0], goal[1]);
+            int h2 = Heuristic(x2, y2, goal[0], goal[1]);
             AddToOpen(x2, y2, g2, h2, open, grid);
         }
     }
@@ -106,19 +112,15 @@ vector<vector<state>> Search(vector<vector<state>> &grid, int start[2], int goal
     int h = Heuristic(x, y, goal[0], goal[1]);
     
     AddToOpen(x, y, g, h, open, grid);
-    
+    cout<< "Open vector size : " << open.size() << "\n";
     while(open.size()>0){
         CellSort(&open);
         auto current = open.back();
         open.pop_back();
         x = current[0];
         y = current[1];
-        try{
-          grid[x][y] = state::Path;
-        }
-        catch (std::exception& e){
-          cout << e.what() << '\n';
-        }
+        
+        grid[x][y] = state::Path;
         
         if((x == goal[0]) && (y == goal[1])){
             cout<< "Goal Reached";
@@ -131,7 +133,6 @@ vector<vector<state>> Search(vector<vector<state>> &grid, int start[2], int goal
     return vector<vector<state>>{};
 }
 
-
 vector<state> parseNewLine(string line){
     istringstream newLine(line);
     vector<state> row;
@@ -143,14 +144,14 @@ vector<state> parseNewLine(string line){
     return row;
 }
 
-
 vector<vector<state>> ReadBoardFile(string path){
     
-    vector<vector<state>> board;
+    vector<vector<state>> board{};
     ifstream ifs;
     ifs.open(path, ifstream::in);
     if (ifs.is_open()){
         string line;
+        //cout<<board.size();
         while(getline(ifs, line)){
             vector<state> row = parseNewLine(line);
             board.push_back(row);
@@ -162,13 +163,14 @@ vector<vector<state>> ReadBoardFile(string path){
 int main(int argc, const char * argv[]) {
     string boardPath = "map.board";
     vector<vector<state>> board = ReadBoardFile(boardPath);
-    int start[2] = {1, 0};
-    int goal[2] = {3, 2};
+    int start[2] = {0, 0};
+    int goal[2] = {3, 4};
     
     vector<vector<state>> solution = Search(board, start, goal);
+    
     PrintBoard(solution);
-    cout<< "\n";
+    cout<<"\n";
+    
     
     return 0;
 }
-
