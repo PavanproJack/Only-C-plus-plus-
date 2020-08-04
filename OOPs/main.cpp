6c14cd9dd86b9c4d51ed3b841189d5542593ae02
PavanKumar Kavvuri <PavanproJack@github.com>
2020-08-04 21:56:19 +0100

    Access specifiers to structures

--- OOPs/main.cpp
+++ OOPs/main.cpp
@@ -9,21 +9,36 @@
 #include <iostream>
 
 struct Date{
-    // Initialise structures.
-    int day{1};
-    int month{1};
-    int year{0};
+    public:
+        int Day(){ return this->day; }
+        void SetDay(int d){ if(d>0) this->day = d; }
+        
+        int Month(){ return this->month; }
+        void SetMonth(int m){ if(m>0) this->month = m; }
+    
+        int Year(){ return this->year; }
+        void SetYear(int y){ if(y>2000) this->year = y; }
+    
+    
+    
+    private:
+        // Initialise structures here
+        int day{1};
+        int month{1};
+        int year{0};
 };
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+     
     Date date;
-    date.day   = 21;
+    date.SetMonth(87);
+    std::cout<< date.Year()<<std::endl;
+    /*date.day   = 21;
     date.month = 01;
-    date.year  = 1995;
-    std::cout<< date.day;
-    assert(date.day == 1);
+    date.year  = 1995;*/
+    //std::cout<< date.day;
+    //assert(date.Day() == 2);
     
     
     return 0;
