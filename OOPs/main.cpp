90750b270448b6e4c74148f165c7ded4ed3fc137
PavanKumar Kavvuri <PavanproJack@github.com>
2020-08-05 16:25:18 +0100

    Adds initialiser lists to initialise variables

--- OOPs/main.cpp
+++ OOPs/main.cpp
@@ -10,16 +10,14 @@
 
 struct Date{
     public:
-        int Day(){ return this->day; }
-        void SetDay(int d){ if(d>0) this->day = d; }
+        int Day();
+        void SetDay(int d);
         
         int Month(){ return this->month; }
         void SetMonth(int m){ if(m>0) this->month = m; }
     
         int Year(){ return this->year; }
         void SetYear(int y){ if(y>2000) this->year = y; }
-    
-    
     
     private:
         // Initialise structures here
@@ -28,19 +26,72 @@ struct Date{
         int year{0};
 };
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+class NewDate{
+    public:
+    NewDate(int d, int m, int y){
+        Day(d);
+    }
+        int Day() { return day_; }
+    void Day(int d);
+    private:
+        int day_{1};
+        int month_{1};
+        int year_{2};
+};
+
+void NewDate::Day(int d){
+    day_ = d;
+}
+
+void Date::SetDay(int d){
+     if (d >= 1 && d <= 31)
+         this->day = d;
+}
+
+
+int Date::Day(){
+    return this->day;
+}
+
+
+class Birthday{
+    public:
+        Birthday(int d, int m, int y);
+        int Day();
+        void SetDay(int d);
+    
+        
+    private:
+        int const day;
+        int const month;
+        int const year;
      
-    Date date;
-    date.SetMonth(87);
-    std::cout<< date.Year()<<std::endl;
-    /*date.day   = 21;
-    date.month = 01;
-    date.year  = 1995;*/
-    //std::cout<< date.day;
-    //assert(date.Day() == 2);
+};
+
+int Birthday::Day(){
+    return this->day;
+}
+
+Birthday::Birthday(int d, int m, int y) : day(d), month(m), year(y){}
+
+
+class Person{
+public:
+    Person(std::string name);
     
+private:
+    std::string s;
+};
+
+Person::Person(std::string d) : s(d){}
+
+
+int main(int argc, const char * argv[]){
     
+    Birthday(21, 01, 1995);
+    Person pavan("Pavan");
+    Person naveen("Naveen");
+
     return 0;
     
 }
