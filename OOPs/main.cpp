62d91785f3e621dc4b15275e4e2ce1c7614e961c
PavanKumar Kavvuri <PavanproJack@github.com>
2020-08-05 17:15:14 +0100

    data encapsulation with invariants

--- OOPs/main.cpp
+++ OOPs/main.cpp
@@ -85,13 +85,51 @@ private:
 
 Person::Person(std::string d) : s(d){}
 
+class validDate{
+public:
+    validDate(int d, int m, int y);
+    void setDate(int d, int m, int y);
+    int daysInMonth(int m, int y);
+    void setDay(int d);
+    void setMonth(int m);
+    void setYear(int y);
+    int day{0}; int month{0}; int year{0};
+};
+
+int validDate::daysInMonth(int m, int y){
+    if(m == 2){ return 29;}
+    else if (m == 4 || m == 6 || m == 9 || m == 11)
+        return 30;
+    else
+        return 31;
+}
+
+void validDate::setDay(int d){ day = d; }
+void validDate::setMonth(int m){ month = m; }
+void validDate::setYear(int y){ year = y; }
+
+void validDate::setDate(int d, int m, int y){
+    if(d >= 1 && d <= daysInMonth(m, y)){
+        setDay(d);
+        setMonth(m);
+        setYear(y);
+    }
+}
+
+validDate::validDate(int dt, int mth, int yr){
+    
+    setDate(dt, mth, yr);
+}
+
 
 int main(int argc, const char * argv[]){
     
-    Birthday(21, 01, 1995);
-    Person pavan("Pavan");
-    Person naveen("Naveen");
-
+    //Birthday(21, 01, 1995);
+    //Person pavan("Pavan");
+    //Person naveen("Naveen");
+    validDate date(21, 01, 1995);
+    
+    assert(date.day == 21);
     return 0;
     
 }
