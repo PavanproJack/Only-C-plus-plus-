1eda594141afe9f75cead7496d198be0954ff343
PavanKumar Kavvuri <PavanproJack@github.com>
2020-08-16 10:43:05 +0100

    Adds pointers to arrays malloc, memset, new, delete functionality

--- Advanced-OOPs/main.cpp
+++ Advanced-OOPs/main.cpp
@@ -12,6 +12,8 @@ using std::cin;
 using std::string;
 using std::endl;
 
+#define PI 3.141529
+
 class Vehicle{
 public:
     //Vehicle(string ff, string cc);
@@ -28,7 +30,7 @@ public:
 
 //Vehicle::Vehicle(string f, string c): fuel(f), color(c){}
 
-class Car : protected Vehicle{
+class Car : public Vehicle{
 public:
     //Car(bool s, string f);
     bool sunroof = false;
@@ -46,13 +48,132 @@ public:
 };
  
 
-class Bicycle : public Car{
+class Bicycle : private byke{
 public:
     bool pedal = true;
     void Print(){
-        Car::Print();
+        cout<< this->color<<endl;
     }
 };
+
+class Animal{
+public:
+    string color = "white";
+    string name = "Pavan";
+    int age = 0;
+};
+
+class Snake : public Animal{
+public:
+    int length = 10;
+    string MakeSound(){
+        return "chchchch Sound" ;
+    }
+};
+
+class Cat : public Animal{
+public:
+    float height = 12.2;
+    string MakeSound(){
+        return "Meow Sound" ;
+    }
+};
+
+/***************Friend Class Declaration*/
+
+class Square{
+public:
+    Square(float ss) : side(ss){}
+    
+private:
+    friend class Rectangle;
+    float side = 23.7;
+};
+
+class Rectangle{
+public:
+    Rectangle(Square sd) : width(sd.side), height(sd.side){}
+    float Area(){
+        return width * height;
+    }
+private:
+    float width = 12.45;
+    float height = 13.37;
+
+};
+
+/***************** PolyMorphism *************/
+// Operator Overloading...
+
+class Point{
+private:
+    int x = 0;
+    int y = 2;
+public:
+    Point(int x_, int y_) : x(x_), y(y_){}
+    
+    void operator > (Point p2){
+        if((this->x + this->y) < (p2.x + p2.y)){
+            cout << "Sum Exceedes"<< endl;
+        }else{
+            cout<< "Sum ....."<< endl;
+        }
+    }
+};
+
+// Polymorphism: Overriding ---- Virtual Fnctions and Abstract Base Classes
+
+class Animal_{  // Abstract base class - for implementing a Pure virtual function
+public:
+    virtual string Talk() const = 0;
+    int size = 0;
+};
+class Cat_ : public Animal_{
+public:
+    string Talk() const{
+        cout << "I make Meowww sound..." << endl;
+        return string("Meow.....");
+    }
+};
+class Dog_ : public Animal_{
+    string Talk() const{
+        cout << "I make bovvv-ovv sound..." << endl;
+        return string("bovvv-ovv.....");
+    }
+};
+
+// Virtual Functions
+
+class Shape {
+    public:
+        Shape() {}
+        virtual double Area() const = 0;
+        virtual double Perimeter() const = 0;
+};
+
+class Rectangle_ : public Shape{
+    public:
+        Rectangle_(double ww, double hh) : width(ww), height(hh){}
+        virtual double Area() const{ return width * height; }
+        virtual double Perimeter() const{ return 2 * ( width + height); }
+        
+    private:
+        double width{0};
+        double height{0};
+    
+};
+
+class Circle_ : public Shape{
+    public:
+        Circle_(double rr) : radius(rr){}
+        virtual double Area() const{ return PI * radius * radius; }
+        virtual double Perimeter() const{ return 2 * ( radius * PI); }
+        
+    private:
+        double radius{0};
+         
+};
+
 
 
 
@@ -68,14 +189,40 @@ int main(int argc, const char * argv[]) {
     car.wheels = 4;
     car.vehicle = "car";*/
     
-    Car x;
+    /*Car x;
     x.Print();
-    
     Bicycle byk;
     byk.Print();
-    
     byke bk;
-    bk.Print();
+    bk.Print();*/
+    
+    /*Snake sm;
+    Cat ct;
+    
+    cout<< "My Length is : " << sm.length << "and I make sound "<< sm.MakeSound()<<endl;
+    
+    cout<< "My Height is : " << ct.height << "and I make sound "<< ct.MakeSound()<<endl;
+    
+    Square sq(4.0);
+    Rectangle rt(sq);
+    
+    cout<< rt.Area()<< endl;*/
+    
+    /*Point p1(30, 32), p2(12, 34);
+    
+    p1 > p2;*/
+    
+
+    Circle_ cv(2);
+    cout<< cv.Area()<< endl;
+    
+    Rectangle_ rv(23, 45.56);
+    cout<< rv.Perimeter() << endl;
+    
+ 
+    
+    
+    
     
     
      
--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -7,45 +7,224 @@
 //
 
 #include <iostream>
-
+#include <cmath>
 using std::cout;
 using std::endl;
 using std::cin;
+using std::string;
+
+class pointerArray{
+public:
+    pointerArray(int size);
+    void initializeArray();
+    void resizeArray(int size);
+    void viewMyArray();
+    
+private:
+    int size;
+    int* arr;
+};
+
+pointerArray :: pointerArray(int sz) : size(sz){}
+
+void pointerArray::initializeArray(){
+    //arr = new int[size];
+    int arr[size];
+    for(int i = 0; i < size; i++){
+        //*(arr + i) = i;
+        arr[i] = i;
+    }
+    cout << "Double Array Created: " << sizeof(arr) << endl;
+}
+
+void pointerArray::viewMyArray(){
+    cout << size << endl;
+    for(int j = 0; j < sizeof(arr) ; j++) {
+        cout << j << " : " << arr[j] << endl;
+    }
+}
+
+void pointerArray::resizeArray(int size){
+    
+    if(size == this->size){
+        cout<< "I don't want to do this bcz....." << endl;
+        return;
+    }
+    double* newArr = new double[size];
+    for(int j = 0; j < size ; j++) {
+        newArr[j] = arr[j];
+    }
+    //this->size = size;
+    
+    cout << "Array Resized to " << sizeof(newArr) << endl;
+   
+}
+
+class deref{
+public:
+    deref(double ang) : angle(ang){}
+    void calcAngles(){
+        sine = std::sin(angle);
+        cosin = std::cos(angle);
+    }
+
+private:
+    double angle{0.0};
+    double sine{0};
+    double cosin{0};
+};
+
+/* Swap Functionality with C++ Templates */
+
+template <typename T>
+void swap(T &var1, T &var2) {
+    var1 += var2;           // add variable2 to variable 1
+    var2  = var1 - var2;    // subtract variable 2 from variable 1 to get new variable 2
+    var1 -= var2;           // subtract new variable 2 from new variable 1 to get newest variable 1
+}
+
+/*void addSpaces(const char* &str) {
+    char *temp = new char(sizeof(str)*2);
+    char *start = temp;
+
+    while (*str != '\0') {
+        *temp++ = *str++;
+        *temp++ = ' ';
+    }
+    str = start;
+}*/
 
 int main(int argc, const char * argv[]) {
     
-    float num = 32;
-    float* ptr;
-    ptr = &num;
-    //std::cout << sizeof(ptr)<<std::endl;
     
-    double values[10];
-    double* d_ptr = values;
-    //cout<< d_ptr << endl;
-    //cout<< values << endl;
+    char* kst = new(std::nothrow) char('C');
+    char str[] = "AAA";
+
     
-    double numbers[5];
-    // Array name automatically contains the address of the first element of the array.
-    double* pNumbers = numbers;
-    // Loop through the next addresses.
-    /*for(int i = 0; i<10; i++){
-        cout<< "values " << i << "address is : "  << pNumbers + i << endl;
-    }*/
+    int* ktr = NULL;
+    ktr = new(std::nothrow) int;
     
-    double testScores[5], sum = 0;
-    double* p_tScore;
-    p_tScore = testScores;
-    double Q;
-    for(int i = 0; i<5; i++){
-        cout<< "Enter score "<< i << endl;
-        cin >> Q;
-        //cout << "Q value is : " << Q;
-        *(p_tScore + i) = Q;
-        sum = sum + *(p_tScore + i);
+    if(!ktr){
+        cout<< "Memory Allocation Failed" << endl;
     }
-    for(int j : testScores){
-        cout << j << endl;
+    else{
+        *ktr = 31;
+        cout<< "Value at the address: pointer is : " << *ktr << endl;
+        cout<< "Pointing to the address : " << ktr << endl;
     }
     
-    return 0;
+    float* arrays = new(std::nothrow) float[10];
+    if(!arrays){
+        cout<< "Memory allocation to this array failed" << endl;
+    }
+    else{
+        for(int i = 0; i < 10; i++){
+                arrays[i] = i * i;
+                cout<<" Address is: " << &arrays[i] << endl;
+                cout<<" Value is: " << arrays[i] << endl;
+        }
+    }
+    double* pointer = nullptr;
+    pointer = new double(23);
+    
+    for(int i = 0; i<4; i++ ){
+        *pointer = ++ *pointer ;
+        cout << "Value at the address: pointer is : " << *pointer << endl;
+    }
+    
+    delete pointer;
+    delete ktr;
+    delete[] arrays;
+    
+    //cout << "New Value at the address: pointer is : " << *pointer << endl;
+    
+    /*std::string str = "Hello World";
+    const char* ptr = str.c_str();
+    std::cout<<"string is: "<<ptr<<std::endl;
+    addSpaces(ptr);
+     
+    std::cout<<"string is: "<<ptr<<std::endl;*/
+    
+    //cout<< str<< endl;
+    
+    //deref df(30);
+    //df.calcAngles();
+    
+    /*float first_var = 18.10;
+    float second_var = 20.22;
+    
+    std::cout << "Calling function swap() -----> " << "\n";
+    swap(first_var, second_var);
+    
+    std::cout << "first_var : " << first_var << "\n";
+    std::cout << "second_var : " << second_var << "\n";*/
+    
+    /*int var1 = 8;
+    double var2 = 8.2;
+
+    int &ref1 = var1;
+    double &ref2 = var2;
+
+    std::cout << ref1 << "\n";
+    std::cout << ref2 << "\n";
+
+    // modify variables
+    var1 = 3;
+    var2 = 9.5;
+
+    std::cout << ref1 << "\n";
+    std::cout << ref2 << "\n";*/
+    
+    /*pointerArray pA(4);
+    pA.initializeArray();
+    pA.viewMyArray();
+    pA.resizeArray(13);
+    pA.viewMyArray();*/
+    
+    /*
+    int* ptr = nullptr;
+    ptr = new int;
+    
+    int g = 334;
+    *ptr = g;
+    
+    // *ptr = 8;
+    cout<< ptr << endl;
+    
+    delete ptr;
+    
+     float num = 32;
+     float* ptr;
+     ptr = &num;
+     //std::cout << sizeof(ptr)<<std::endl;
+     
+     double values[10];
+     double* d_ptr = values;
+     //cout<< d_ptr << endl;
+     //cout<< values << endl;
+     
+     double numbers[5];
+     // Array name automatically contains the address of the first element of the array.
+     double* pNumbers = numbers;
+     // Loop through the next addresses.
+     for(int i = 0; i<10; i++){
+         cout<< "values " << i << "address is : "  << pNumbers + i << endl;
+     }
+     
+     double testScores[5], sum = 0;
+     double* p_tScore;
+     p_tScore = testScores;
+     double Q;
+     for(int i = 0; i<5; i++){
+         cout<< "Enter score "<< i << endl;
+         cin >> Q;
+         //cout << "Q value is : " << Q;
+         *(p_tScore + i) = Q;
+         sum = sum + *(p_tScore + i);
+     }
+     for(int j : testScores){
+         cout << j << endl;
+     }
+     */
+    return 1;
 }
