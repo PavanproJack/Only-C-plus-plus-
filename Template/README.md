d37080ef0947506131fa303b295ce19eee35d181
Pavan Kumar <kavvuripavankumar@gmail.com>
2022-09-24 11:12:33 +0100

    brushed up over the basics

---
+++ Template/README.md
@@ -0,0 +1,5 @@
+
+
+std::cerr -> Printing errors to the console.
+std::clog -> Printing log messages to the console.
+
--- Template/main.cpp
+++ Template/main.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<string>
+
 
 
 consteval int get_value(){
@@ -9,6 +11,31 @@ consteval int get_value(){
 int main(){
     constexpr int value = get_value();
     std::cout<< "Value is : " << value << std::endl;
+
+    int age {27}, full_age {32};
+    std::string name;
+    std::string fullname;
+    
+    // std::cin>> 
+    std::cout<< "age is : " << age << std::endl;
+
+    // for prinitng  errors to console
+    std::cerr << "Error: sth went wrong here : " << std::endl;
+    // for printing logs to console
+    std::clog << "A sample warning message here " << std::endl;
+
+    // for getting multiple inputs in a single line
+    // std::cin >> name >> age;     // This has problem if you input any space in the name. 
+    // std::cin >> "Enter Name : " >> name >> "Enter Age : " >> age; 
+
+    std::getline(std::cin, fullname);
+    std::cin >> full_age;
+
+
+    // std::cout << "Hey " << name << " you are : " << age << "years old " << std::endl;
+    std::cout << "Hey " << fullname << " you are : " << full_age << "years old " << std::endl;
+
+
     return 0;
 }
 
